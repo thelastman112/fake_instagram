beee2dbb4be0b8cc90d3fbf9e3deb5bce757bbf3
thelastman112 <rizkymaestro12@gmail.com>
2022-10-18 20:18:21 +0700

    commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram/utils/colors.dart';
+import 'package:instagram/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +11,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +31,7 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Flexible(child: Container(), flex: 1),
+            Flexible(child: Container(), flex: 2),
             //svg image
             SvgPicture.asset(
               'assets/ic_instagram.svg',
@@ -31,10 +42,62 @@ class _LoginScreenState extends State<LoginScreen> {
               height: 64,
             ),
             //text fields for email
-
+            TextFieldInput(
+                textEditingController: _emailController,
+                hintText: 'Enter your Email',
+                textInputType: TextInputType.emailAddress),
+            const SizedBox(
+              height: 24,
+            ),
             //text fields for password
+            TextFieldInput(
+                isPass: true,
+                textEditingController: _passwordController,
+                hintText: 'Enter your Password',
+                textInputType: TextInputType.text),
+            const SizedBox(
+              height: 24,
+            ),
             //login button
+            InkWell(
+              child: Container(
+                child: const Text('Log In'),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4))),
+                  color: blueColor,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Flexible(child: Container(), flex: 2),
             //transition to sign up
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: const Text("don't have an account? "),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    child: const Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),

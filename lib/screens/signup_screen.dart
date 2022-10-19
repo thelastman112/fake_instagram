6f03f13b73d75d9a3e9d478a5be36cc8cc9e310b
thelastman112 <rizkymaestro12@gmail.com>
2022-10-19 11:28:36 +0700

    commit

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram/resources/auth_methods.dart';
 import 'package:instagram/utils/colors.dart';
 import 'package:instagram/widgets/text_field_input.dart';
 
@@ -52,8 +53,21 @@ class _SingupScreenState extends State<SignupScreen> {
                   radius: 64,
                   backgroundImage: NetworkImage(
                       'https://images.unsplash.com/photo-1657214059175-53cb22261d38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'),
+                ),
+                Positioned(
+                  bottom: -10,
+                  left: 80,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.add_a_photo,
+                    ),
+                  ),
                 )
               ],
+            ),
+            const SizedBox(
+              height: 24,
             ),
             //text fields for username
             TextFieldInput(
@@ -62,7 +76,7 @@ class _SingupScreenState extends State<SignupScreen> {
               textInputType: TextInputType.text,
             ),
             const SizedBox(
-              height: 12,
+              height: 24,
             ),
             //text fields for email
             TextFieldInput(
@@ -70,7 +84,7 @@ class _SingupScreenState extends State<SignupScreen> {
                 hintText: 'Enter your Email',
                 textInputType: TextInputType.emailAddress),
             const SizedBox(
-              height: 12,
+              height: 24,
             ),
             //text fields for password
             TextFieldInput(
@@ -79,7 +93,7 @@ class _SingupScreenState extends State<SignupScreen> {
                 hintText: 'Enter your Password',
                 textInputType: TextInputType.text),
             const SizedBox(
-              height: 12,
+              height: 24,
             ),
             //text fields for Bio
             TextFieldInput(
@@ -88,10 +102,19 @@ class _SingupScreenState extends State<SignupScreen> {
               textInputType: TextInputType.text,
             ),
             const SizedBox(
-              height: 12,
+              height: 24,
             ),
             //Sign in button
             InkWell(
+              onTap: () async {
+                String res = await AuthMethods().singUpUser(
+                  email: _emailController.text,
+                  password: _passwordController.text,
+                  username: _usernameController.text,
+                  bio: _bioController.text,
+                );
+                print(res);
+              },
               child: Container(
                 child: const Text('Sign In'),
                 width: double.infinity,
@@ -105,7 +128,7 @@ class _SingupScreenState extends State<SignupScreen> {
               ),
             ),
             SizedBox(
-              height: 12,
+              height: 24,
             ),
             Flexible(child: Container(), flex: 2),
             //transition to sign up

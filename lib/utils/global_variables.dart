1503513d0e24c8784e9cd51b6ccd9731a7f5059c
thelastman112 <rizkymaestro12@gmail.com>
2022-11-11 10:24:18 +0700

    melihat data profil pemakai

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,16 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/rendering.dart';
 import 'package:instagram/screens/add_post_screen.dart';
 import 'package:instagram/screens/feed_screen.dart';
 import 'package:instagram/screens/profile_screen.dart';
 import 'package:instagram/screens/search_screen.dart';
+import 'package:instagram/screens/profile_screen.dart';
 
 const webScreenSize = 600;
 
-const homeScreenItems = [
-  FeedScreen(),
-  SearchScreen(),
-  AddPostScreen(),
-  Text('notif'),
-  ProfileScreen(),
+List<Widget> homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notif'),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
 ];
